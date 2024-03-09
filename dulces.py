2523b88b5a4049263031d7304eeb2844ef1accde
NicoXtreme <79645805+NicoXtreme@users.noreply.github.com>
2024-03-09 14:56:24 -0500

    Update dulces.py
    Pasos updateados

--- dulces.py
+++ dulces.py
@@ -3,28 +3,32 @@ def suma_maxima(matriz):
     columnas = len(matriz[0])
 
     if filas == 0 or columnas == 0:
-        return []
+        return [], 0  
 
     maximos = [0] * filas
-    for i, fila in enumerate(matriz):
+    pasos = 0  
+    for fila in matriz:
         n = len(fila)
         if n == 0:
-            maximos[i] = 0
+            pasos += 1  
+            continue  
         elif n == 1:
-            maximos[i] = fila[0]
+            maximos[pasos] = fila[0]
+            pasos += 1
+            continue  
         else:
             incluido = fila[0]
             excluido = 0
 
             for j in range(1, n):
                 nueva_incluido = excluido + fila[j]
-
                 excluido = max(incluido, excluido)
                 incluido = nueva_incluido
 
-            maximos[i] = max(incluido, excluido)
+            maximos[pasos] = max(incluido, excluido)
+            pasos += 1  
 
-    return maximos
+    return maximos, pasos  
 
 matriz = [[1, 8, 2, 1, 9],
           [1, 7, 3, 5, 2],
@@ -32,11 +36,13 @@ matriz = [[1, 8, 2, 1, 9],
           [8, 4, 7, 9, 1],
           [7, 1, 3, 1, 6]]
 
-resultado_obtenido = suma_maxima(matriz)
-print("Sumas maximas de cada fila:", resultado_obtenido)
+resultado_obtenido, pasos = suma_maxima(matriz)
+print("Sumas máximas de cada fila:", resultado_obtenido)
+print("Pasos realizados:", pasos)
 
-# Aplicar el proceso nuevamente a la nueva lista obtenida
 nueva_lista = resultado_obtenido
-nuevo_resultado = suma_maxima([nueva_lista])
+nuevo_resultado, pasos_nuevo = suma_maxima([nueva_lista])
 
-print("Maximos dulces posibles teniendo en cuenta la restriccion:", nuevo_resultado)
+print("Máximos dulces posibles teniendo en cuenta la restricción: (Respuesta)", nuevo_resultado)
+print("Pasos realizados en el nuevo cálculo:", pasos_nuevo)
+
